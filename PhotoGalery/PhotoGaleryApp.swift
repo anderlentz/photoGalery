fb3f6b549a5701fa6dda7471acf40fad89aa30fb
Anderson Lentz <anderlentz@gmail.com>
2022-03-12 20:11:25 -0300

    Initial Commit

---
+++ PhotoGalery/PhotoGaleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGaleryApp.swift
+//  PhotoGalery
+//
+//  Created by Anderson Lentz on 12/03/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGaleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
